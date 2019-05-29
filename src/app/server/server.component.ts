744d57b35d62e41bb732d078a9e71ed3843ae7a4
URIEL ALEXIS FARIZELI FIORI <URIEL.FIORI@cptm.sp.gov.br>
2019-05-29 14:52:03 -0300

    added ngStyle directive to server component

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -9,11 +9,17 @@ export class ServerComponent implements OnInit {
   serverId = 10;
   serverStatus = 'offline';
 
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
+
+  ngOnInit() {}
+
   getServerStatus() {
     return this.serverStatus;
   }
 
-  constructor() {}
-
-  ngOnInit() {}
+  getColor() {
+    return this.serverStatus === 'online' ? 'green' : 'red';
+  }
 }
