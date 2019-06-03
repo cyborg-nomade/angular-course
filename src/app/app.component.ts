4952ef9c301b85c38f458cd39a73539c6e1efcb3
URIEL ALEXIS FARIZELI FIORI <URIEL.FIORI@cptm.sp.gov.br>
2019-06-03 16:29:06 -0300

    cockpit element emitting events (app back working)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,23 @@ export class AppComponent {
   serverElements = [
     { type: 'server', name: 'Test', content: 'This is a test' }
   ];
+
+  onServerAdded(serverData: { serverName: string; serverContent: string }) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+  onBlueprintAdded(bluetprintData: {
+    blueprintName: string;
+    blueprintContent: string;
+  }) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: bluetprintData.blueprintName,
+      content: bluetprintData.blueprintContent
+    });
+  }
 }
