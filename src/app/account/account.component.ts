98c35768555585dbfa54ff7be126056f4002b745
URIEL ALEXIS FARIZELI FIORI <URIEL.FIORI@cptm.sp.gov.br>
2019-06-11 17:15:50 -0300

    using events in services to comm btw comps

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -22,5 +22,6 @@ export class AccountComponent implements OnInit {
   onSetTo(status: string) {
     this.accountsService.updateStatus(this.id, status);
     // this.loggingService.logStatusChange(status);
+    this.accountsService.statusUpdated.emit(status);
   }
 }
