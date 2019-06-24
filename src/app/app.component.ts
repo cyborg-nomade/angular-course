462739fa75092b8e9bfcdad7f85f8275e214d0b5
URIEL ALEXIS FARIZELI FIORI <URIEL.FIORI@cptm.sp.gov.br>
2019-06-24 17:02:20 -0300

    assignment finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent {
+  @ViewChild('refForm', { static: true }) form: NgForm;
+  subscriptionTypes = ['Basic', 'Advanced', 'Pro'];
+  defaultSubscription = 'Advanced';
+  submitted = false;
+  user = {
+    email: '',
+    subscription: '',
+    password: ''
+  };
+
+  onSubmit() {
+    this.submitted = true;
+    this.user.email = this.form.value.email;
+    this.user.subscription = this.form.value.subscription;
+    this.user.password = this.form.value.password;
+    console.log(this.user);
+
+    this.form.reset();
+  }
+}
