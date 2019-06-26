45d96ea054653ce915f5a1b58446c7a28ca5ff41
URIEL ALEXIS FARIZELI FIORI <URIEL.FIORI@cptm.sp.gov.br>
2019-06-26 18:30:49 -0300

    creating a custum validator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,15 @@ import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 export class AppComponent implements OnInit {
   genders = ['male', 'female'];
   signupForm: FormGroup;
+  forbiddenUsernames = ['Chris', 'Anna'];
 
   ngOnInit() {
     this.signupForm = new FormGroup({
       userData: new FormGroup({
-        username: new FormControl(null, Validators.required),
+        username: new FormControl(null, [
+          Validators.required,
+          this.forbidUsernames.bind(this)
+        ]),
         email: new FormControl(null, [Validators.required, Validators.email])
       }),
       gender: new FormControl('female'),
@@ -28,5 +32,12 @@ export class AppComponent implements OnInit {
   onAddHobby() {
     const control = new FormControl(null, Validators.required);
     (this.signupForm.get('hobbies') as FormArray).push(control);
+  }
+
+  forbidUsernames(control: FormControl): { [s: string]: boolean } {
+    if (this.forbiddenUsernames.indexOf(control.value) !== -1) {
+      return { nameForbidden: true };
+    }
+    return null;
   }
 }
