b9f33cc96d35a7b0d0a7c8e000d6a7516bba278a
URIEL ALEXIS FARIZELI FIORI <URIEL.FIORI@cptm.sp.gov.br>
2019-07-04 15:45:14 -0300

    reverse and sort pipes created with CLI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { ReversePipe } from './reverse.pipe';
+import { SortPipe } from './sort.pipe';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ReversePipe, SortPipe],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
