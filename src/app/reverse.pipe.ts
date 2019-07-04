fa7b74973a2070276c29eab4323fa00a37cdb213
URIEL ALEXIS FARIZELI FIORI <URIEL.FIORI@cptm.sp.gov.br>
2019-07-04 16:41:43 -0300

    exercise completed

--- src/app/reverse.pipe.ts
+++ src/app/reverse.pipe.ts
@@ -4,9 +4,10 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'reverse'
 })
 export class ReversePipe implements PipeTransform {
-
-  transform(value: any, args?: any): any {
-    return null;
+  transform(input: string): string {
+    return input
+      .split('')
+      .reverse()
+      .join('');
   }
-
 }
--- src/app/sort.pipe.ts
+++ src/app/sort.pipe.ts
@@ -4,9 +4,16 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'sort'
 })
 export class SortPipe implements PipeTransform {
-
-  transform(value: any, args?: any): any {
-    return null;
+  transform(input: Array<any>): Array<any> {
+    const sortedArray: Array<any> = input.sort((obj1, obj2) => {
+      if (obj1.name > obj2.name) {
+        return 1;
+      }
+      if (obj1.name < obj2.name) {
+        return -1;
+      }
+      return 0;
+    });
+    return sortedArray;
   }
-
 }
